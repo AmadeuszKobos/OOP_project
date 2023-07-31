5897c76f4003b3063395b2c453d11f41292c5a63
Amadeusz <Acer@LAPTOP-DCKI4AHO>
2023-07-31 18:05:36 +0200

    Class method Static method instances from csv

--- main.py
+++ main.py
@@ -1,34 +1,62 @@
+import csv
+
+
 def color(text, color_code):
     return f"\033[{color_code}m{text}\033[0m"
 
 
 class Item:
     company_name = "Chihuahua"
+    all_items = []
 
     def __init__(self, name: str = "", price: float = 0, quantity=0):
+        # Run validation for received arguments
         assert price >= 0, f"Price: {price} is not greater than or equal to zero"
         assert quantity >= 0, f"Quantity: {quantity} is not greater than or equal to zero"
 
+        # Assign to self object
         self.name = name
         self.price = price
         self.quantity = quantity
+
+        Item.all_items.append(self)
 
         print(color("New item created!!!", "33"))
 
     def calculate_total_price(self):
         print(f"The prize of all {self.name}s is equal to {self.price * self.quantity}")
 
+    def __repr__(self):
+        return f"Item('{self.name}', '{self.price}', '{self.quantity}')"
 
-# Creating instances of a class Item
-phone = Item()
-phone.name = "Phone"
-phone.price = 100
-phone.quantity = 5
+    @classmethod
+    def initiate_from_csv(cls):
+        with open('items.csv', 'r') as f:
+            reader = csv.DictReader(f)
+            items = list(reader)
 
-laptop = Item(name="Laptop", price=1000, quantity=10)
+        for item in items:
+            Item(
+                name=item.get('name'),
+                price=float(item.get('price')),
+                quantity=int(item.get('quantity'))
+            )
 
-# Calling items methods
-phone.calculate_total_price()
-laptop.calculate_total_price()
+    @staticmethod
+    def is_integer(num):
+        if isinstance(num, float):
+            return num.is_integer()
+        elif isinstance(num, int):
+            return True
+        else:
+            return False
 
-print(color(f"This is called from class {Item.company_name}, this is called from instance {phone.company_name}", "34"))
+
+for instance in Item.all_items:
+    print(f'{color("Name: ", "34")} {instance.name}, {color("Price: ", "34")} {instance.price}, {color("Quantity: ", "34")} {instance.quantity}')
+
+
+Item.initiate_from_csv()
+print(Item.all_items)
+
+print(color(Item.is_integer(5.0), "34"))
